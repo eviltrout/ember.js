b12397e5519f7f054f9d503b50efb10b00159387
Robert Jackson <robert.w.jackson@me.com>
2014-04-21 10:58:17 -0400

    Use broccoli-file-mover package.

--- Brocfile.js
+++ Brocfile.js
@@ -8,38 +8,9 @@ var mergeTrees = require('broccoli-merge-trees');
 var defeatureify = require('broccoli-defeatureify');
 var concat = require('broccoli-concat');
 var uglifyJavaScript = require('broccoli-uglify-js');
-var helpers = require('broccoli-kitchen-sink-helpers')
-var Writer = require('broccoli-writer');
+var moveFile = require('broccoli-file-mover');
 
 var globalWrapInEval = false;
-
-var Writer = require('broccoli-writer');
-
-Mover.prototype = Object.create(Writer.prototype);
-Mover.prototype.constructor = Mover;
-function Mover (inputTree, options) {
-  if (!(this instanceof Mover)) return new Mover(inputTree, options);
-
-  this.inputTree = inputTree;
-  this.srcFile   = options.srcFile;
-  this.destFile  = options.destFile;
-  this.copy      = options.copy;
-};
-
-Mover.prototype.write = function (readTree, destDir) {
-  var self = this
-
-  return readTree(this.inputTree).then(function (srcDir) {
-    helpers.copyRecursivelySync(srcDir, destDir);
-    helpers.copyRecursivelySync(
-      path.join(destDir, self.srcFile),
-      path.join(destDir, self.destFile));
-
-    if (!self.copy) { fs.unlinkSync(path.join(destDir, self.srcFile)); }
-  })
-};
-
-var moveFile = Mover;
 
 function defeatureifyConfig(stripDebug) {
   var configJson = JSON.parse(fs.readFileSync("features.json").toString());
