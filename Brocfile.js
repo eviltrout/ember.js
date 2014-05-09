481ca97e34b3cbae124801717888a68fd71552cf
Robert Jackson <robert.w.jackson@me.com>
2014-05-08 20:23:55 -0400

    Move packages into separate files.
    Allows sharing between `Brocfile.js` and tests.

--- Brocfile.js
+++ Brocfile.js
@@ -150,21 +150,7 @@ var vendoredPackages = {
   'route-recognizer': vendoredPackage('route-recognizer'),
 };
 
-var packages = {
-  'container':                  {trees: null,  requirements: []},
-  'ember-metal':                {trees: null,  vendorRequirements: ['backburner']},
-  'ember-debug':                {trees: null,  requirements: ['ember-metal'], skipTests: true},
-  'ember-runtime':              {trees: null,  vendorRequirements: ['rsvp'], requirements: ['container', 'ember-metal']}, 
-  'ember-views':                {trees: null,  requirements: ['ember-runtime']},
-  'ember-extension-support':    {trees: null,  requirements: ['ember-application']},
-  'ember-testing':              {trees: null,  requirements: ['ember-application', 'ember-routing']},
-  'ember-handlebars-compiler':  {trees: null,  requirements: ['ember-views']},
-  'ember-handlebars':           {trees: null,  vendorRequirements: ['metamorph'], requirements: ['ember-views', 'ember-handlebars-compiler']},
-  'ember-routing':              {trees: null,  vendorRequirements: ['router', 'route-recognizer'],
-                                               requirements: ['ember-runtime', 'ember-views', 'ember-handlebars']},
-  'ember-application':          {trees: null,  requirements: ['ember-routing']},
-  'ember':                      {trees: null,  requirements: ['ember-application']}
-};
+var packages = require('./lib/packages');
 
 function es6Package(packageName) {
   var pkg = packages[packageName],
